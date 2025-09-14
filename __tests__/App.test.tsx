01d59ef687d08b170c0a82f0f33fbdf4accc394c
survivatech <kumar@survivatechnologies.com>
2025-09-14 21:00:10 +0530

    test apps

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,8 +6,25 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+describe('App Component', () => {
+  test('renders correctly', () => {
+    const tree = ReactTestRenderer.create(<App />);
+    expect(tree).toBeTruthy();
+  });
+
+  test('component renders without crashing', () => {
+    expect(() => {
+      ReactTestRenderer.create(<App />);
+    }).not.toThrow();
+  });
+
+  test('component exists', () => {
+    expect(App).toBeDefined();
+    expect(typeof App).toBe('function');
+  });
+
+  test('component can be instantiated', () => {
+    const instance = ReactTestRenderer.create(<App />);
+    expect(instance).toBeDefined();
   });
 });
